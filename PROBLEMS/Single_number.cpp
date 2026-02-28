f7e0c738a06bdf77eabc77477d13a0ddb1db6270
Piyush <bishtpiyush610@gamil.com>
2026-02-28 13:06:58 +0530

    Modified the code of Single number

--- PROBLEMS/Single_number.cpp
+++ PROBLEMS/Single_number.cpp
@@ -2,12 +2,16 @@
 #include <vector>
 using namespace std;
 
-int main(){
-    vector <int> v={8,5,7,8,5};
+int single_num(vector <int> &v){
     int ans=0;
     for (int val : v){
         ans^= val;
     }
-    cout<< ans;
+    return ans;
+}
+
+int main(){
+    vector <int> v={8,5,7,8,5};
+    cout<< single_num(v);
     return 0;
 }
